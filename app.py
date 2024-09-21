ee80e7d8adfee5aa0edcc91dcb1a42d38d33b7dd
Emmanuel-Chukwudebere <immanuelonyinyechi@gmail.com>
2024-09-21 13:34:49 +0100

    db update

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from datetime import date
 app = Flask(__name__)
 
 # Database (MariaDB) configuration using environment variable
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'mysql+pymysql://ceo:CEOKachifo2024@kachifo.cteuykcg0zmb.eu-north-1.rds.amazonaws.com/kachifo')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'mysql+pymysql://username:password@localhost/kachifo_db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Initialize database and cache
